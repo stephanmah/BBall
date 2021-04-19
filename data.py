346518f57c1b43396d07eb90fef8c96c50c510d3
Stephan Mah <35859912+stephanmah@users.noreply.github.com>
2021-04-19 06:09:10 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,7 @@
 from basketball_reference_scraper.teams import get_roster, get_team_stats, get_opp_stats, get_roster_stats, get_team_misc
+from basketball_reference_scraper.players import get_stats, get_game_logs, get_player_headshot
 
-lakers = get_team_stats("LAL",2019)
-print(lakers)
+lebronStats = get_stats('LeBron James', stat_type='advanced',playoffs = False,career=False)
+kdStats = get_stats('Kevin Durant', stat_type='advanced', playoffs=False, career=False)
+
+print(lebronStats,kdStats)
